5c64cfe5476dd20de4dccc5510959c50b7058a8c
Ubaidullah <121395297+UbaidullahTech@users.noreply.github.com>
2023-02-27 20:05:40 +0500

    add lahore

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 function App() {
   const [data,setData] = useState({})
-  const [location, setLocation] = useState('')
+  const [location, setLocation] = useState('Lahore')
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=19f811528d1aab4095e197501690885c&units=metric`
   const searchLocation = (event) =>{
     if (event.key === 'Enter') {
