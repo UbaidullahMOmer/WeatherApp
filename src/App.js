9a919c4e6e12ed4fa17f2f2a10c302f332ca6de7
Ubaidullah <121395297+UbaidullahTech@users.noreply.github.com>
2023-02-27 20:08:09 +0500

    add space

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
         value={location}
         onChange={event => setLocation(event.target.value)}
         onKeyPress={searchLocation}
-        placeholder='Enter Location'
+        placeholder='Enter Location'  
         type="text"/>
       </div>
       <div className="container">
