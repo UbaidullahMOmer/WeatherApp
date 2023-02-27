17eef45a91899f3619301cd1dfbd12071b2d381a
Ubaidullah <121395297+UbaidullahTech@users.noreply.github.com>
2023-02-27 13:06:24 +0500

    ADD SOME HTML

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import React, {useState} from "react";
+import axios from "axios";
 
 function App() {
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=19f811528d1aab4095e197501690885c`
   return (
-    <div className="App">
-      <h1>Ubaidullah</h1>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65°F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            <p>12 MPH</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
